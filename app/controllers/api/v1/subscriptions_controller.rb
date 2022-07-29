26df766db71133357b5e7b6d179696b624635551
Sherman Au <shermau13@gmail.com>
2022-07-28 18:33:22 -0600

    feat/test: subscription#create
    subscription#create endpoint and tests

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -8,6 +8,15 @@ class Api::V1::SubscriptionsController < ApplicationController
     render json: SubscriptionSerializer.new(subscriptions), status: :ok
   end
 
+  def create
+    subscription = Subscription.new(subscription_params)
+    if subscription.save
+      render json: SubscriptionSerializer.new(subscription), status: :created
+    else
+      render json: { errors: subscription.errors.full_messages }, status: :bad_request
+    end
+  end
+
   private
 
   def subscription_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       resources :teas, only: [:create]
 
       resources :customers, only: [:create] do
-        resources :subscriptions, only: [:index]
+        resources :subscriptions, only: %i[index create]
       end
     end
   end
